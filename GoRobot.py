3392079edd4cef88a2fcca64c301ed4e04e3e50c
jfranklyn <59396137+jfranklyn@users.noreply.github.com>
2020-03-22 14:29:38 -0500

    Add files via upload

--- GoRobot.py
+++ GoRobot.py
@@ -7,6 +7,7 @@ print(dev)
 
 import Robot
 from HCSR04Sensor import distance
+#from neopixel_rpi_python import *
 
 # Set the trim offset for each motor (left and right).    
 LEFT_TRIM   = 0
@@ -21,20 +22,24 @@ robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 # Use the arrow keys to control the robot
 def key_input(event):
-    #print ('Key:', event.value)
-    sleep_time = 0.030
+    #print ('Key:', event.value, event.code)
+    #sleep_time = 0.030
     if event.code == 103:
         print ("Forward")
         robot.forward(150)
+        sleep_time = 0.030
     elif event.code == 108:
         print  ("Back")
         robot.backward(150)
+        sleep_time = 0.030
     elif event.code == 106:
         print ("right")
         robot.right(100, 0.25)
+        sleep_time = 0.030
     elif event.code == 105:
         print ("left")
-        robot.left(100, 0.25) 
+        robot.left(100, 0.25)
+        sleep_time = 0.030
 # enter = stop        
     elif event.code == 28:
         print ("stop")
@@ -45,14 +50,17 @@ def key_input(event):
 try:
     while (distance() > 10):
         for event in dev.read_loop():
+# start LED's blinking. sleep for 5 ms after each cycle           
+            #rainbow_cycle(0.005)
             curDis = distance()
             print('Current distance in CM: ', curDis)
-            if curDis < 10:
+            if curDis < 15:
                 print('Too Close!, Reversing ')
                 robot.backward(100, 0.5)
                 robot.stop() 
             key_input(event)            
 
+            
 except KeyboardInterrupt:
     print ('Interrupted')
     sys.exit(0)
