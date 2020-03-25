188cfaefc79eb0e07ebccbaa431e444f6c2c6106
jfranklyn <59396137+jfranklyn@users.noreply.github.com>
2020-03-24 20:11:08 -0500

    added distance checking code
    changed code to check distance before moving
    changed the key_input function to look for the event.code and event.type = 1

--- GoRobot.py
+++ GoRobot.py
@@ -10,8 +10,8 @@ from HCSR04Sensor import distance
 #from neopixel_rpi_python import *
 
 # Set the trim offset for each motor (left and right).    
-LEFT_TRIM   = 0
-RIGHT_TRIM  = -2
+LEFT_TRIM   = -1
+RIGHT_TRIM  = 0
 
 # Create an instance of the robot with the specified trim values.
 # Not shown are other optional parameters:
@@ -20,25 +20,49 @@ RIGHT_TRIM  = -2
 #  - right_id: The ID of the right motor, default is 2.
 robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)    
 
+def check_distance():
+# check distance b4 moving    
+    curDis = distance()
+    print('Current distance in CM: ', curDis)
+    if distance() < 15:
+        print('Too Close!, Reversing ')
+        robot.backward(100, 0.5)
+        robot.stop()
+    if distance() < 15:
+        print('Too Close!, Reversing ')
+        robot.backward(100, 0.5)
+        robot.stop()
+    if distance() < 15:
+        print('Too Close!, Reversing ')
+        robot.backward(100, 0.5)
+        robot.stop()
+    else:
+        print('WAY Too Close!, Stopping ')
+        robot.stop()
+        
 # Use the arrow keys to control the robot
 def key_input(event):
     #print ('Key:', event.value, event.code)
     #sleep_time = 0.030
-    if event.code == 103:
+    if (event.code == 103 and event.value == 0):
+        check_distance()
         print ("Forward")
         robot.forward(150)
         sleep_time = 0.030
-    elif event.code == 108:
+    elif (event.code == 108 and event.value == 0):
+        check_distance()
         print  ("Back")
         robot.backward(150)
         sleep_time = 0.030
-    elif event.code == 106:
+    elif (event.code == 106 and event.value == 0):
+        check_distance()
         print ("right")
-        robot.right(100, 0.25)
+        robot.right(100, 0.15)
         sleep_time = 0.030
-    elif event.code == 105:
+    elif (event.code == 105  and event.value == 0):
+        check_distance()
         print ("left")
-        robot.left(100, 0.25)
+        robot.left(100, 0.15)
         sleep_time = 0.030
 # enter = stop        
     elif event.code == 28:
@@ -48,18 +72,11 @@ def key_input(event):
         pass
 
 try:
-    while (distance() > 10):
         for event in dev.read_loop():
 # start LED's blinking. sleep for 5 ms after each cycle           
             #rainbow_cycle(0.005)
-            curDis = distance()
-            print('Current distance in CM: ', curDis)
-            if curDis < 15:
-                print('Too Close!, Reversing ')
-                robot.backward(100, 0.5)
-                robot.stop() 
-            key_input(event)            
-
+            check_distance()
+            key_input(event)
             
 except KeyboardInterrupt:
     print ('Interrupted')
